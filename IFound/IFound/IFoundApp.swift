5e29197694dd0c1708dddb86475832775421c4db
edvildt <edgarvildt@gmail.com>
2022-12-20 20:37:35 +0200

    Initial commit

---
+++ IFound/IFound/IFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  IFoundApp.swift
+//  IFound
+//
+//  Created by Edgar Vildt on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct IFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
